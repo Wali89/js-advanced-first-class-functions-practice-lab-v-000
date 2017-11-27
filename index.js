d917f4d8e9b3699d267047e687728363ae861676
Wali Rahman <wali.rahman89@yahoo.com>
2017-11-27 13:07:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
-const logDriverNames = function (arr) {
-  console.log (arr.forEach(callback));
+const logDriverNames = function (drivers) {
+  console.log (drivers.forEach(callback));
 }
